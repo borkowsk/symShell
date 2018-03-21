56645cec336925c5c5a6b2c9ab6fc0a7759ae7c7
Wojciech T. Borkowski <37558456+borkowsk@users.noreply.github.com>
2018-03-21 23:27:29 +0100

    Najstarsze znalezione

---
+++ SYMSHELL/src/Unix/icon.h
@@ -0,0 +1,12 @@
+/* icon.h - header                  */
+/************************************/
+/* ONLY FOR symshx11.c and releated */
+
+#ifndef __icon_h
+#define __icon_h
+
+extern int WB_icon_bitmap_width;
+extern int WB_icon_bitmap_height;
+extern char WB_icon_bitmap_bits[];
+
+#endif
