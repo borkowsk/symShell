5538aa18864d6694fe4bb5dc554c48d14d52e7ed
borkowsk <wborkowsk@gmail.com>
2018-12-30 17:40:54 +0100

    Próba zaimplementowania Menu dla X11 obsługiwanego przez pomocniczą aplikacje z Motiff

---
+++ WBRTM/MenuProj/Unused/sshdefs.c
@@ -0,0 +1,5 @@
+/* Domyslne parametry wymagane dla prawidlowego linkowania pod x11 */
+/*******************************************************************/
+#include "sshmenud.h"
+int 			ssh_menu_trace=0; /* For debuging purposes - external accesible */
+char*        	ssh_menu_options="-fg black -bg gray";
