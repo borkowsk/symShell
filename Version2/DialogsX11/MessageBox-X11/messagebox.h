b5d2179daf8f3ce9db13fe7b44e5f3eb4965d2df
Wojciech T. Borkowski <wborkowsk@gmail.com>
2022-07-06 12:45:32 +0200

    Drobne porządki

---
+++ Version2/DialogsX11/MessageBox-X11/messagebox.h
@@ -0,0 +1,17 @@
+#ifndef MESSAGEBOX_X11_MESSAGEBOX_H
+#define MESSAGEBOX_X11_MESSAGEBOX_H
+
+#include <X11/Xlib.h>
+
+typedef struct Button{
+    wchar_t *label;
+    int result;
+}Button;
+
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int Messagebox(const char* title, const wchar_t* text, const Button* buttons, int numButtons);
+
+#endif //MESSAGEBOX_X11_MESSAGEBOX_H
